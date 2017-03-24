b21b7db15d51b4944e0a453a7ed92b244607a8e9
mdavila0482@gmail.com <mdavila0482@gmail.com>
2017-03-23 22:38:49 -0600

    segundocommit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+	skip_before_action :verify_authenticity_token
   def x
+  end
+
+  def save_user
+  	User.new(name: params[:name], email: params[:email], age: params[:age]).save
+  	redirect_to pages_x_path, notice: "El usuario se ha creado correctamente"
   end
 end
